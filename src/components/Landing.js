6896524c072c3a1d74cfc9747f44bd87cc06302b
Jared Moats <JaredMoats@outlook.com>
2019-04-30 18:14:20 -0400

    Added extra css styling for <br />

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,8 +6,7 @@ const Landing = () => {
     <div className="landing-container">
       <h1>
         West Virginia's <span className="jl-yellow">first</span> eSports
-        <br />
-        and Virtual Reality Gaming Center
+        <br /> and Virtual Reality Gaming Center
       </h1>
       <div className="button-container">
         <a className="button is-jl-red">Games</a>
